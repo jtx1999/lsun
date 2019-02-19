0f3102e001b6a19d47938276afa29af296039add
Tianxing Jiang <tj258@cornell.edu>
2019-02-19 14:37:26 -0500

    Change the file structure

--- data.py
+++ data.py
@@ -41,12 +41,12 @@ def export_images(db_path, out_dir, flat=False, limit=-1):
         cursor = txn.cursor()
         for key, val in cursor:
             if not flat:
-                image_out_dir = join(out_dir, '/'.join(str(key[:6])))
+                image_out_dir = join(out_dir, str(key[:6].decode()))
             else:
                 image_out_dir = out_dir
             if not exists(image_out_dir):
                 os.makedirs(image_out_dir)
-            image_out_path = join(image_out_dir, key.decode() + '.webp')
+            image_out_path = join(image_out_dir, str(key.decode()) + '.webp')
             with open(image_out_path, 'wb') as fp:
                 fp.write(val)
             count += 1
